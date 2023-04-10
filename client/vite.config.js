242e8450be54cc3bcb8ceb401d00274d6088ae4a
peter varga <pkvarg@yahoo.se>
2023-04-10 19:54:56 +0200

    MainCarousel Vite Corr

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import replace from '@rollup/plugin-replace'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    replace({
+      'require.context': () => null,
+      preventAssignment: true,
+    }),
+  ],
+
   server: {
     port: 3000,
     // proxy: {
